ea64b607fb91d68f67193ecdcda688114fa24eb3
Karen Hernandez <131834614+Vanessaherndz@users.noreply.github.com>
2024-05-03 20:51:10 -0600

    cambios en main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,53 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+class ColorChangeWidget extends StatefulWidget {
+  const ColorChangeWidget({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ColorChangeWidgetState createState() => _ColorChangeWidgetState();
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class _ColorChangeWidgetState extends State<ColorChangeWidget> {
+  Color _currentColor = Colors.red;
+
+  void _toggleColor() {
+    setState(() {
+      _currentColor = _currentColor == Colors.blue ? Colors.red : Colors.blue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return GestureDetector(
+      onTap: _toggleColor,
+      child: Container(
+        width: 150,
+        height: 90,
+        color: _currentColor,
+        child: const Center(
+          child: Text(
+            'Presionar',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Diferente color'),
+      ),
+      body: const Center(
+        child: ColorChangeWidget(),
+      ),
+    ),
+  ));
 }
